18377378c14e43cf3d0fe6ea1d76d715b6f51057
洪诚 <18502729006@163.com>
2019-01-13 13:11:04 +0800

    主界面ui更新添加侧滑菜单

--- app/src/main/java/com/xp/dc/xpdc/activity/MainActivity.kt
+++ app/src/main/java/com/xp/dc/xpdc/activity/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.xp.dc.xpdc.activity
 
 import android.content.Intent
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBar
 import android.util.TypedValue
+import android.view.MenuItem
 import android.view.View
 import com.baidu.mapapi.map.Marker
 import com.baidu.mapapi.model.LatLng
 import com.baidu.mapapi.search.geocode.*
-import com.example.hongcheng.common.base.BaseActivity
+import com.example.hongcheng.common.base.BasicActivity
 import com.example.hongcheng.common.util.ScreenUtils
 import com.example.hongcheng.common.util.StringUtils
 import com.example.hongcheng.common.util.ViewUtils
@@ -15,53 +17,43 @@ import com.xp.dc.xpdc.R
 import com.xp.dc.xpdc.application.BaseApplication
 import com.xp.dc.xpdc.location.AppLocationUtils
 import com.xp.dc.xpdc.location.XPLocation
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_map.*
 import kotlinx.android.synthetic.main.layout_app_common_title.*
 
 
-class MainActivity : BaseActivity(), View.OnClickListener, AppLocationUtils.XPLocationListener {
-
+class MainActivity : BasicActivity(), View.OnClickListener, AppLocationUtils.XPLocationListener {
     companion object {
-        private const val NOW : Int = 0
-        private const val APPOINTMENT : Int = 1
+        private const val NOW: Int = 0
+        private const val APPOINTMENT: Int = 1
     }
 
-    private var CURRENT_STATE : Int = NOW
+    private var CURRENT_STATE: Int = NOW
 
     private var lastPosition: XPLocation? = null
     private var curPosMark: Marker? = null          // 位置图标标记
 
-    override fun getTitleLayoutResId(): Int {
-        return R.layout.layout_app_common_title
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_main
     }
 
-    override fun initTitleView(view: View) {
+    override fun initView() {
         setSupportActionBar(tb_app_common)
         ScreenUtils.setWindowStatusBarColor(this, resources.getColor(R.color.white))
         ScreenUtils.setLightStatusBar(this, true)
-        tb_app_common.setNavigationIcon(R.mipmap.icon_user)
-        tb_app_common.setNavigationOnClickListener { }
         val actionBar: ActionBar? = supportActionBar
         actionBar?.title = ""
         tv_app_common_right.visibility = View.VISIBLE
-    }
+        actionBar?.setHomeAsUpIndicator(R.mipmap.icon_user)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
-    override fun getMessageLayoutResId(): Int {
-        return 0
-    }
-
-    override fun initMessageView(view: View) {
-    }
-
-    override fun getBodyLayoutResId(): Int {
-        return R.layout.activity_map
-    }
-
-
-    override fun initBodyView(view: View) {
         tv_call_car_pre.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_size_13))
         tv_call_car_pre.setBackgroundDrawable(null)
 
+        nv_main.getHeaderView(0).findViewById<View>(R.id.ll_nv_main_head_edit).setOnClickListener(this)
+        nv_main.getHeaderView(0).findViewById<View>(R.id.ll_nv_main_head_order).setOnClickListener(this)
+        nv_main.getHeaderView(0).findViewById<View>(R.id.ll_nv_main_head_edit_pw).setOnClickListener(this)
+        nv_main.getHeaderView(0).findViewById<View>(R.id.ll_nv_main_head_about).setOnClickListener(this)
         tv_app_common_right.setOnClickListener(this)
         tv_call_car_now.setOnClickListener(this)
         tv_call_car_pre.setOnClickListener(this)
@@ -77,6 +69,15 @@ class MainActivity : BaseActivity(), View.OnClickListener, AppLocationUtils.XPLo
             mv_main.addStartMarker(it.target)
             getAddressInfo(it.target)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> dl_main.openDrawer(GravityCompat.START)
+            else -> {
+            }
+        }
+        return true
     }
 
     override fun onLocateSuccess(xpLocation: XPLocation?) {
@@ -105,14 +106,16 @@ class MainActivity : BaseActivity(), View.OnClickListener, AppLocationUtils.XPLo
             override fun onGetReverseGeoCodeResult(reverseGeoCodeResult: ReverseGeoCodeResult) {
                 val sb = StringBuilder()
                 val poiList = reverseGeoCodeResult.poiList
-                if(poiList != null && !poiList.isEmpty()) {
+                if (poiList != null && !poiList.isEmpty()) {
                     sb.append(poiList[0].name)
-                } else if(!StringUtils.isEmpty(reverseGeoCodeResult.addressDetail.street)) {
+                } else if (!StringUtils.isEmpty(reverseGeoCodeResult.addressDetail.street)) {
                     sb.append(reverseGeoCodeResult.addressDetail.street)
-                } else if(!StringUtils.isEmpty(reverseGeoCodeResult.addressDetail.town)){
-                    sb.append(reverseGeoCodeResult.addressDetail.district).append(reverseGeoCodeResult.addressDetail.town)
+                } else if (!StringUtils.isEmpty(reverseGeoCodeResult.addressDetail.town)) {
+                    sb.append(reverseGeoCodeResult.addressDetail.district)
+                        .append(reverseGeoCodeResult.addressDetail.town)
                 } else {
-                    sb.append(reverseGeoCodeResult.addressDetail.city).append(reverseGeoCodeResult.addressDetail.district)
+                    sb.append(reverseGeoCodeResult.addressDetail.city)
+                        .append(reverseGeoCodeResult.addressDetail.district)
                 }
                 tv_current_position.text = sb.toString()
             }
@@ -123,27 +126,56 @@ class MainActivity : BaseActivity(), View.OnClickListener, AppLocationUtils.XPLo
     override fun onClick(v: View?) {
         if (ViewUtils.isFastClick()) return
         when (v?.id) {
+            R.id.ll_nv_main_head_edit
+            -> {
+                dl_main.closeDrawers()
+            }
+            R.id.ll_nv_main_head_order -> {
+                startActivity(Intent(this, OrderShowActivity::class.java))
+                dl_main.closeDrawers()
+            }
+
+            R.id.ll_nv_main_head_edit_pw -> {
+                dl_main.closeDrawers()
+            }
+
+            R.id.ll_nv_main_head_about -> {
+                dl_main.closeDrawers()
+            }
+
             R.id.tv_app_common_right
             -> {
                 startActivity(Intent(this, OrderShowActivity::class.java))
             }
             R.id.tv_call_car_now
             -> {
-                if(CURRENT_STATE == APPOINTMENT) {
+                if (CURRENT_STATE == APPOINTMENT) {
                     CURRENT_STATE = NOW
-                    tv_call_car_now.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.normal_text_size))
+                    tv_call_car_now.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        resources.getDimension(R.dimen.normal_text_size)
+                    )
                     tv_call_car_now.setBackgroundResource(R.drawable.tv_gray_conner)
-                    tv_call_car_pre.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_size_13))
+                    tv_call_car_pre.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        resources.getDimension(R.dimen.text_size_13)
+                    )
                     tv_call_car_pre.setBackgroundDrawable(null)
                 }
             }
             R.id.tv_call_car_pre
             -> {
-                if(CURRENT_STATE == NOW) {
+                if (CURRENT_STATE == NOW) {
                     CURRENT_STATE = APPOINTMENT
-                    tv_call_car_pre.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.normal_text_size))
+                    tv_call_car_pre.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        resources.getDimension(R.dimen.normal_text_size)
+                    )
                     tv_call_car_pre.setBackgroundResource(R.drawable.tv_gray_conner)
-                    tv_call_car_now.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_size_13))
+                    tv_call_car_now.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        resources.getDimension(R.dimen.text_size_13)
+                    )
                     tv_call_car_now.setBackgroundDrawable(null)
                 }
             }
