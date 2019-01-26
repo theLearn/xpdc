85798e045c8d974cf332771342464f156bcec9b1
洪诚 <18502729006@163.com>
2019-01-26 11:19:42 +0800

    更新glide配置

--- common/src/main/java/com/example/hongcheng/common/util/ImageLoadUtils.java
+++ common/src/main/java/com/example/hongcheng/common/util/ImageLoadUtils.java
@@ -2,54 +2,46 @@ package com.example.hongcheng.common.util;
 
 import android.graphics.*;
 import android.widget.ImageView;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
-import com.bumptech.glide.request.RequestOptions;
 import com.example.hongcheng.common.R;
 
 public final class ImageLoadUtils {
 
-    private ImageLoadUtils(){}
+    private ImageLoadUtils() {
+    }
 
     public static void bindImageUrl(ImageView view, String url) {
-        RequestOptions options = new RequestOptions()
-                .error(R.drawable.img_default)
-                .placeholder(R.drawable.img_default)
-                .priority(Priority.HIGH)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .centerCrop();
-
-        Glide.with(view.getContext()).applyDefaultRequestOptions(options).load(url)
-                .transition(DrawableTransitionOptions.withCrossFade()).into(view);
+        bindImageUrl(view, url, R.drawable.img_default);
     }
 
     public static void bindImageUrlForCycle(ImageView view, String url) {
-        RequestOptions options = new RequestOptions()
-                .placeholder(R.drawable.img_default)
-                .error(R.drawable.img_default)
-                .priority(Priority.HIGH)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .centerCrop()
-                .transform(new CircleCrop());
-
-        Glide.with(view.getContext()).applyDefaultRequestOptions(options).load(url)
-                .transition(DrawableTransitionOptions.withCrossFade()).into(view);
+        bindImageUrlForCycle(view, url, R.drawable.img_default);
     }
 
     public static void bindImageUrlForRound(ImageView view, String url) {
-        RequestOptions options = new RequestOptions()
-                .placeholder(R.drawable.img_default)
-                .error(R.drawable.img_default)
-                .priority(Priority.HIGH)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .centerCrop()
-                .transform(new RoundedCorners(30));
+        bindImageUrlForRound(view, url, R.drawable.img_default);
+    }
 
-        Glide.with(view.getContext()).applyDefaultRequestOptions(options).load(url)
+    public static void bindImageUrl(ImageView view, String url, int resId) {
+        GlideApp.with(view.getContext()).load(url).error(resId)
+                .placeholder(resId)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade()).into(view);
+    }
+
+    public static void bindImageUrlForCycle(ImageView view, String url, int resId) {
+        GlideApp.with(view.getContext()).load(url).placeholder(resId)
+                .error(resId)
+                .transform(new CircleCrop())
+                .transition(DrawableTransitionOptions.withCrossFade()).into(view);
+    }
+
+    public static void bindImageUrlForRound(ImageView view, String url, int resId) {
+        GlideApp.with(view.getContext()).load(url).placeholder(resId)
+                .error(resId)
+                .transform(new RoundedCorners(30))
                 .transition(DrawableTransitionOptions.withCrossFade()).into(view);
     }
 
